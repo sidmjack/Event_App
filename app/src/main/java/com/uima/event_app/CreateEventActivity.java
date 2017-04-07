476593c9e323456ff582336d203d6524414b8027
econno14 <edmundConnor@EJs-MacBook-Pro.local>
2017-04-06 20:41:23 -0400

    Initial commit

---
+++ app/src/main/java/com/uima/event_app/CreateEventActivity.java
@@ -0,0 +1,13 @@
+package com.uima.event_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_event);
+    }
+}
---
+++ app/src/main/java/com/uima/event_app/EventPageActivity.java
@@ -0,0 +1,13 @@
+package com.uima.event_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EventPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_page);
+    }
+}
