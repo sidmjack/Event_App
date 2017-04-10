5c91958834c0d301cf5df601da9f012d93660fcd
econno14 <edmundConnor@EJs-MacBook-Pro.local>
2017-04-10 17:19:48 -0400

    EJ's initial UI push, pretty basic stuff, colors and strings not formatted

--- app/src/main/java/com/uima/event_app/NavigationActivity.java
+++ app/src/main/java/com/uima/event_app/NavigationActivity.java
@@ -80,17 +80,17 @@ public class NavigationActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_discover) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_drafts) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_eboard) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_map) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_live) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_past) {
 
         }
 
