e96dc4d4485dbfd521de581338810ddb681b1e19
cnoellekb <cnoellekb@gmail.com>
2017-04-16 00:35:59 -0400

    Added my changes back into the new structure, can't figure out how to tabs to show up on a fragment... but the event adapter and imageview seem to be fine.

--- app/src/main/java/com/uima/event_app/MainActivity.java
+++ app/src/main/java/com/uima/event_app/MainActivity.java
@@ -16,6 +16,8 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private MyLiveEventsFragment liveFrag;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,14 +25,14 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,6 +42,11 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        liveFrag = new MyLiveEventsFragment();
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, liveFrag).commit();
     }
 
     @Override
