5a3070d763e7c500682bf244758ee679c3b28f2e
Brian Cueto <brianmbcueto@gmail.com>
2017-04-18 20:07:05 -0400

    fixed merge conflicts

--- app/src/main/java/com/uima/event_app/MainActivity.java
+++ app/src/main/java/com/uima/event_app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.uima.event_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
