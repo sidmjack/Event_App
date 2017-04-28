20614a3da7728cea918dd104bae97386153d8d5f
sidneyjackson <werty6400@gmail.com>
2017-04-28 18:56:08 -0400

    Authentication works with email/password and gmails (once we can register accounts...farily certain)

--- app/src/main/java/com/uima/event_app/LoginActivity.java
+++ app/src/main/java/com/uima/event_app/LoginActivity.java
@@ -3,16 +3,42 @@ package com.uima.event_app;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener{
 
     SharedPreferences myPrefs;
+    GoogleApiClient mGoogleApi;
+    private static final String TAG = "LoginActivity";
+    private static final int RC_SIGN_IN = 9001;
+
+    private FirebaseAuth mAuth;
+
+    // Views
+    private EditText mEmailField;
+    private EditText mPasswordField;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,47 +48,84 @@ public class LoginActivity extends AppCompatActivity {
         Context context = getApplicationContext();
         myPrefs = PreferenceManager.getDefaultSharedPreferences(context);
 
+        // Google Stuff
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleApi = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        SignInButton gSignIn = (SignInButton) findViewById(R.id.google_sign_in_button);
+        gSignIn.setOnClickListener(this);
+
+        // Regular User Stuff
+
+        // Views
+        mEmailField = (EditText) findViewById(R.id.login_email_field);
+        mPasswordField = (EditText) findViewById(R.id.login_password_field);
+
+        // Buttons
+        findViewById(R.id.signin_button).setOnClickListener(this);
+        findViewById(R.id.register_button).setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+
     }
 
-    private boolean isEmailEntered() {
-        EditText emailField = (EditText) findViewById(R.id.login_email_field);
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
 
-        if (!emailField.getText().toString().equals("")) {
-            return true;
+    public void signInTapped() {
+
+        if (!validateForm()) {
+            return;
         }
 
-        return false;
-    }
-
-    private boolean isPasswordEntered() {
+        EditText emailField = (EditText) findViewById(R.id.login_email_field);
         EditText passwordField = (EditText) findViewById(R.id.login_password_field);
 
-        if (!passwordField.getText().toString().equals("")) {
-            return true;
-        }
+        mAuth.signInWithEmailAndPassword(emailField.getText().toString(), passwordField.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            goToMain();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
 
-        return false;
+                        // [START_EXCLUDE]
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Task not successful",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        // [END_EXCLUDE]
+                    }
+                });
+
     }
 
+    private void goToMain() {
+        SharedPreferences.Editor editor = myPrefs.edit();
+        editor.putBoolean("logged_in", true);
+        editor.apply();
 
-    public void signInTapped(View view) {
-
-        if (!isEmailEntered()) {
-            Toast.makeText(getApplicationContext(),"Invalid email!", Toast.LENGTH_SHORT).show();
-
-        } else if (!isPasswordEntered()) {
-            Toast.makeText(getApplicationContext(),"Invalid password!", Toast.LENGTH_SHORT).show();
-
-        } else {
-            SharedPreferences.Editor editor = myPrefs.edit();
-            editor.putBoolean("logged_in", true);
-            editor.apply();
-
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-            finish();
-        }
-
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 
     public void registerTapped(View view) {
@@ -71,10 +134,72 @@ public class LoginActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.google_sign_in_button) {
+            Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApi);
+            startActivityForResult(signInIntent, RC_SIGN_IN);
+        } else if (v.getId() == R.id.signin_button) {
+            signInTapped();
+        }
+    }
 
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+    }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if(requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
 
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            GoogleSignInAccount acct = result.getSignInAccount();
 
+            //statusTextView.setText("Hello, " + acct.getDisplayName());
+
+            SharedPreferences.Editor editor = myPrefs.edit();
+            editor.putBoolean("logged_in", true);
+            editor.apply();
+
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+
+        } else if (result.getStatus().isInterrupted()) {
+            // DO NOTHING!
+        } else {
+            Toast.makeText(getApplicationContext(), "Account not registered.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = mEmailField.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            mEmailField.setError("Required.");
+            valid = false;
+        } else {
+            mEmailField.setError(null);
+        }
+
+        String password = mPasswordField.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            mPasswordField.setError("Required.");
+            valid = false;
+        } else {
+            mPasswordField.setError(null);
+        }
+
+        return valid;
+    }
 }
