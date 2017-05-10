f33413d2480ac0298f2c7b27d026c9bab9fe2f65
edmundConnor <econno14@jhu.edu>
2017-05-10 01:37:20 -0400

    select event pictures

--- app/src/main/java/com/uima/event_app/ProfileActivity.java
+++ app/src/main/java/com/uima/event_app/ProfileActivity.java
@@ -3,12 +3,17 @@ package com.uima.event_app;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -29,11 +34,17 @@ public class ProfileActivity extends AppCompatActivity {
     TextView facebookTextView;
     TextView websiteTextView;
     TextView zipcodeTextView;
+    ImageView profileImage;
 
     private FirebaseDatabase database;
     private FirebaseAuth mAuth;
     private FirebaseUser currentUser;
     private DatabaseReference currentUserRef;
+
+    private int PICK_IMAGE_REQUEST = 111;
+    private Uri filePath;
+
+    private static int RESULT_LOAD_IMG = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,9 +56,21 @@ public class ProfileActivity extends AppCompatActivity {
         facebookTextView = (TextView) findViewById(R.id.profile_fb);
         websiteTextView = (TextView) findViewById(R.id.profile_website);
         zipcodeTextView = (TextView) findViewById(R.id.profile_zip);
+        profileImage = (ImageView) findViewById(R.id.profile_image);
 
         Context context = getApplicationContext();
         myPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+
+        Button changeImgButton = (Button) findViewById(R.id.profile_change_pic);
+
+        changeImgButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, RESULT_LOAD_IMG);
+            }
+        });
     }
 
     @Override
@@ -79,6 +102,25 @@ public class ProfileActivity extends AppCompatActivity {
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // To clean up all activities
         startActivity(intent);
         finish();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            filePath = data.getData();
+
+            try {
+                //getting image from gallery
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+
+                //Setting image to ImageView
+                profileImage.setImageBitmap(bitmap);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     /**
